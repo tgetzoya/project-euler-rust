c3f4b61237bcd1ead35bba4db5706874fd9a29d8
Thomas Getzoyan <tgetzoya@gmail.com>
2024-01-27 22:35:49 -0600

    Problem 17 finished.

--- src/main.rs
+++ src/main.rs
@@ -9,52 +9,55 @@ fn main() {
     let mut result;
 
     result = problems::problem1::problem1();
-    println!("Problem 1: {:15}, {:?}", result.0, result.1);
+    println!("Problem 001: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem2::problem2();
-    println!("Problem 2: {:15}, {:?}", result.0, result.1);
+    println!("Problem 002: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem3::problem3();
-    println!("Problem 3: {:15}, {:?}", result.0, result.1);
+    println!("Problem 003: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem4::problem4();
-    println!("Problem 4: {:15}, {:?}", result.0, result.1);
+    println!("Problem 004: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem5::problem5();
-    println!("Problem 5: {:15}, {:?}", result.0, result.1);
+    println!("Problem 005: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem6::problem6();
-    println!("Problem 6: {:15}, {:?}", result.0, result.1);
+    println!("Problem 006: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem7::problem7();
-    println!("Problem 7: {:15}, {:?}", result.0, result.1);
+    println!("Problem 007: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem8::problem8();
-    println!("Problem 8: {:15}, {:?}", result.0, result.1);
+    println!("Problem 008: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem9::problem9();
-    println!("Problem 9: {:15}, {:?}", result.0, result.1);
+    println!("Problem 009: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem10::problem10();
-    println!("Problem 10: {:15}, {:?}", result.0, result.1);
+    println!("Problem 010: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem11::problem11();
-    println!("Problem 11: {:15}, {:?}", result.0, result.1);
+    println!("Problem 011: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem12::problem12();
-    println!("Problem 12: {:15}, {:?}", result.0, result.1);
+    println!("Problem 012: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem13::problem13();
-    println!("Problem 13: {:15}, {:?}", result.0, result.1);
+    println!("Problem 013: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem14::problem14();
-    println!("Problem 14: {:15}, {:?}", result.0, result.1);
+    println!("Problem 014: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem15::problem15();
-    println!("Problem 15: {:15}, {:?}", result.0, result.1);
+    println!("Problem 015: {:15}, {:?}", result.0, result.1);
 
     result = problems::problem16::problem16();
-    println!("Problem 16: {:15}, {:?}", result.0, result.1);
+    println!("Problem 016: {:15}, {:?}", result.0, result.1);
+
+    result = problems::problem17::problem17();
+    println!("Problem 017: {:15}, {:?}", result.0, result.1);
 
     println!("Total run time: {:?}", start.elapsed());
 }
---
+++ src/problems/problem17.rs
@@ -0,0 +1,98 @@
+use std::ops::{Add, Rem};
+use std::time::{Duration, Instant};
+
+const ONES: [&str; 20] = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+const TENS: [&str; 10] = [
+    "zero",
+    "ten",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+const ORDERS: [&str; 6] = [
+    "thousand",
+    "million",
+    "billion",
+    "trillion",
+    "quadrillion",
+    "quintillion",
+];
+
+pub fn problem17() -> (u128, Duration) {
+    let start = Instant::now();
+    let mut value= 0;
+
+    for idx in 1..1001 {
+        value = value + number_to_word(idx, 0).len();
+    }
+
+    assert_eq!(value, 21124);
+    return (value as u128, start.elapsed());
+}
+
+fn number_to_word(num: u64, order: i8) -> String {
+    let mut result: String;
+
+    match num {
+        0..=19 => result = ONES[num as usize].to_owned(),
+        20..=99 => {
+            result = String::from(TENS[(num / 10) as usize]);
+
+            if num % 10 > 0 {
+                result = result.add(ONES[(num % 10) as usize]);
+            }
+        }
+        100..=999 => {
+            result = String::from(ONES[(num / 100) as usize])
+                .add("hundred");
+
+            let remainder = num % 100;
+
+            if remainder > 0 {
+                if order <= 0 {
+                    result = result.add("and");
+                }
+
+                result = result.add(&*number_to_word(remainder, 0));
+            }
+        }
+        _ => {
+            result = number_to_word(num / 1000, order + 1)
+                .add(ORDERS[order as usize]);
+
+            let remainder = num.rem(1000);
+
+            if remainder > 0 {
+                result = result.add(&*number_to_word(remainder, order));
+            }
+        }
+    }
+
+    return result;
+}
